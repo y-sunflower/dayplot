e7eaad0e965d3d42d0ec525d64bf7d5f46e09408
Joseph Barbier <79746670+JosephBARBIERDARNAL@users.noreply.github.com>
2025-03-25 09:48:46 +0100

    simple changes week start + linter

--- dayplot/main.py
+++ dayplot/main.py
@@ -31,7 +31,7 @@ def calendar(
     vcenter: Optional[float] = None,
     boxstyle: Union[str, matplotlib.patches.BoxStyle] = "square",
     ax: Optional[matplotlib.axes.Axes] = None,
-    week_start_on: str = "Sunday",
+    week_starts_on: str = "Sunday",
     **kwargs,
 ) -> List[matplotlib.patches.Rectangle]:
     """
@@ -111,8 +111,8 @@ def calendar(
         A matplotlib axes. If None, plt.gca() will be used. It is advisable to make
         this explicit to avoid unexpected behaviour, particularly when manipulating a
         figure with several axes.
-    week_start_on
-        The starting day of the week, which can be specified as a string ("Sunday", "Monday", 
+    week_starts_on
+        The starting day of the week, which can be specified as a string ("Sunday", "Monday",
         ..., "Saturday"). Defaults to "Sunday".
     kwargs
         Any additional arguments that will be passed to matplotlib.patches.FancyBboxPatch. For example,
@@ -180,12 +180,22 @@ def calendar(
 
     if len(dates) == 0 or len(values) == 0:
         raise ValueError("`dates` and `values` cannot be empty.")
-    
-    day_names = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    try:
-        week_start_on = day_names.index(week_start_on.capitalize())
-    except ValueError:
-        raise ValueError(f"Invalid start_day string: {week_start_on}. Must be one of {day_names}.")
+
+    day_names = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ]
+    if week_starts_on not in day_names:
+        raise ValueError(
+            f"Invalid start_day string: {week_starts_on}. Must be one of {day_names}."
+        )
+
+    week_starts_on = day_names.index(week_starts_on.capitalize())
 
     date_counts = defaultdict(float)
     for d, v in zip(dates, values):
@@ -221,12 +231,14 @@ def calendar(
     data_for_plot = []
     for d in full_range:
         days_from_start = (d - start_date).days
-        start_date_weekday = (start_date.weekday() - week_start_on) % 7 # Adjusted weekday
+        start_date_weekday = (
+            start_date.weekday() - week_starts_on
+        ) % 7  # Adjusted weekday
         week_index = (days_from_start + start_date_weekday) // 7
-        day_of_week = (d.weekday() - week_start_on) % 7 # Adjusted day of week
+        day_of_week = (d.weekday() - week_starts_on) % 7  # Adjusted day of week
         count = date_counts.get(d, 0)
         data_for_plot.append((week_index, day_of_week, count))
-        
+
     total_weeks = (end_date - start_date).days // 7 + 1
 
     if ax is None:
@@ -326,7 +338,7 @@ def calendar(
 
     ticks = [0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5]
     labels = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
-    adjusted_labels = labels[week_start_on:] + labels[:week_start_on]
+    adjusted_labels = labels[week_starts_on:] + labels[:week_starts_on]
     for y_tick, day_label in zip(ticks, adjusted_labels):
         ax.text(-day_x_margin, y_tick, day_label, **day_text_style)
 
@@ -345,7 +357,7 @@ if __name__ == "__main__":
         df["values"],
         start_date="2024-01-01",
         end_date="2024-12-31",
-        week_start_on='Sunday',
+        week_starts_on="Sunday",
     )
     fig.savefig("test.png", dpi=300, bbox_inches="tight")
     plt.close()
